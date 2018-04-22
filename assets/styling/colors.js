8a32170588eb88cfd97f9ae8a9145ce1577ebe1d
Lucas Cordina <lucas.cordina92@gmail.com>
2018-04-22 17:21:48 +0200

    binding test

--- assets/styling/colors.js
+++ assets/styling/colors.js
@@ -2,6 +2,7 @@ const brandColors = {
   primaryColor: '#42A5F5',
   defaultBackground: '#F7F7F7',
   headingTextColor: '#fff',
+  borderColor: '#ddd',
 };
 
 export default brandColors;
--- components/header/HeaderView.js
+++ components/header/HeaderView.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
 import style from './HeaderViewStyle';
 
 export default class HeaderView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentListName: 'My List',
+      searchValue: '',
+    };
+
+    this.handleSearchUpdate = this.handleSearchUpdate.bind(this);
+  }
+
+  handleSearchUpdate(e) {
+    console.log(e);
+    this.setState(() => ({ currentListName: this.state.searchValue }));
+    console.log(this.state);
+  }
+
   render() {
     return (
       <View style={style.header}>
-        <Text style={style.headerText}>MarketMate</Text>
+        <Text style={style.headerText}>{this.state.currentListName}</Text>
+        <TextInput
+          style={style.searchBar}
+          value={this.state.searchValue}
+          onChangeText={text => this.setState({ currentListName: text })}
+        />
       </View>
     );
   }
